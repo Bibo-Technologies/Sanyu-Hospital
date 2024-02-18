0095057b6f923cdb52e0394450b152a2201f5825
Bibo Technologies <148161719+Bibo-Technologies@users.noreply.github.com>
2024-02-18 16:18:47 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -192,3 +192,26 @@ function closePopup() {
     // Open the link in a new window or redirect the current window
     window.open(whatsappLink, '_blank');
 }
+
+$(document).ready(function(){
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
